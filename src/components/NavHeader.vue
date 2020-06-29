5c710d59806155167e9faee6298a788cf353094e
wangyamin <1151629213@qq.com>
2020-06-30 06:38:27 +0800

    NavHeader组件

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,14 +1,161 @@
 <template>
-    
+    <div class="header">
+        <div class="nav-topbar">
+            <div class="container">
+                <div class="topbar-menu">
+                    <a href="javascript:;">小米商城</a>
+                    <a href="javascript:;">MUI</a>
+                    <a href="javascript:;">云服务</a>
+                    <a href="javascript:;">协议规则</a>
+                </div>
+                <div class="topbar-user">
+                    <a href="javascript:;">王亚敏</a>
+                    <a href="javascript:;">登录</a>
+                    <a href="javascript:;">退出</a>
+                    <a href="javascript:;">我的订单</a>
+                    <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-header">
+            <div class="container">
+                <div class="header-logo">
+                    <a href="/#/index"></a>
+                </div>
+                <div class="header-menu">
+                    <div class="item-menu">
+                        <span>小米手机</span>
+                    </div>
+                    <div class="item-menu">
+                        <span>RedMi红米</span>
+                    </div>
+                    <div class="item-menu">
+                        <span>电视</span>
+                    </div>
+                    
+                </div>
+                <div class="header-search">
+                    <div class="wrapper">
+                        <input type="text" placeholder="家电">
+                        <a href="javascript:;"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    name: 'nav-header'
+    name:'nav-header',
 }
 </script>
 
-<style>
+<style lang="scss">
+    // @import '../assets/scss/reset.scss';
+    @import '../assets/scss/mixin.scss';
+    @import '../assets/scss/config.scss';
+    .nav-topbar{
+        height: 40px;
+        line-height: 40px;
+        background-color: $colorB;
+        color: $colorE;
+        .container{
+            width:1226px;
+            margin-left: auto;
+            margin-right: auto;
+            @include flex();
+            a{
+                display:inline-block;
+                color:$colorE;
+                margin-right: 16px;
+            }
+            .my-cart{
+                width: 110px;
+                background-color: $colorA;
+                text-align: center;
+                color: $colorG;
+                margin-right: 0;
+                .icon-cart{
+                    display: inline-block;
+                    @include bgImg(16px,12px,'/imgs/icon-cart-checked.png');
+                    margin-right: 4px;
+                }
+            }
 
+        }
+    }  
+    .nav-header{
+        .container{
+            width:1226px;
+            height: 112px;
+            margin-right: auto;
+            margin-left: auto;
+            @include flex();
+        }
+        .header-logo{
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            overflow: hidden;
+            background: $colorA;
+            a{
+                display: inline-block;
+                width: 110px;
+                height: 55px;
+                &:before{
+                    content: " ";
+                    @include bgImg(55px,55px,'/imgs/mi-logo.png',55px);
+                    transition:all .2s;
+                }
+                &:after{
+                    content: ' ';
+                    @include bgImg(55px,55px,'/imgs/mi-home.png',55px);
+                }
+                &:hover:before{
+                    margin-left: -55px;
+                    transition: all .2s;
+                }
+            }
+        }
+        .header-menu{
+            display:inline-block;
+            width: 644px;
+            padding-left: 210px;
+            .item-menu{
+                display: inline-block;
+                color:$colorB;
+                font-weight: bold;
+                font-size: $fontI;
+                line-height: 112px;
+                margin-right: 20px;
+                span{
+                    cursor: pointer;
+                }
+            }
+
+        }
+        .header-search{
+            width: 320px;
+            .wrapper{
+                height: 50px;
+                border: 1px solid $colorH;
+                input{
+                    width: 264px;
+                    height: 50px;
+                    padding-left: 10px;
+                    border: none;
+                    box-sizing: border-box;
+                    border-right: 1px solid $colorH;
+                    color: $colorB;
+                }
+                a{
+                    @include bgImg(18px,18px,'/imgs/icon-search.png');
+                    margin-left: 18px;
+                    vertical-align: middle;
+                }
+            }
+        }
+    }  
 </style>
 
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,6 +6,6 @@
 
 <script>
 export default {
-    name:'index'
+    name:'index',
 }
 </script>
