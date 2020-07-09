47a510403256f50e284ed16ba22a2693b0359cc8
wangyamin <1151629213@qq.com>
2020-07-09 23:20:31 +0800

    alipay支付宝支付页面

--- src/pages/alipay.vue
+++ src/pages/alipay.vue
@@ -1,11 +1,43 @@
 <template>
-    <div class="alipay">
-        alipay
+    <div class="ali-pay">
+        <loading v-show="loading"></loading>
+        <div class="form" v-html="content"></div>
     </div>
 </template>
 
 <script>
+import Loading from '../components/Loading';
 export default {
-    name:'alipay'
+    name:'alipay',
+    components:{
+        Loading
+    },
+    data(){
+        return {
+            orderId:this.$route.query.orderId,
+            loading:false,
+            content:'',
+        }
+    },
+    mounted(){
+        this.paySubmit();
+    },
+    methods:{
+        paySubmit(){
+            this.axios.post('/pay',{
+                orderId:this.orderId,
+                orderName:'Vue高仿小米商城',
+                amount:0.01,
+                payType:1 //1:支付宝 2:微信
+            }).then((res) => {
+                this.l = res
+                this.content = res.content;
+                setTimeout(() => {
+                    //获取当前页面的第一个表单
+                    document.forms[0].submit();
+                },100);
+            })
+        }
+    }
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -58,7 +58,7 @@ export default new Router({
                     component: () => import('./pages/orderPay.vue')
                 },
                 {
-                    path:'/alipay',
+                    path:'alipay',
                     name:'alipay',
                     component: () => import('./pages/alipay.vue')
                 }
